f50ef80ab019c73eaea0332795eb8cb88c5b7904
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2025-06-04 16:05:13 +0300

    add

--- lib/mini_pro/counter_pro.dart
+++ lib/mini_pro/counter_pro.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CounterPro extends StatelessWidget {
-  const CounterPro({super.key});
+class CounterPro extends StatefulWidget {
+  CounterPro();
 
+@override
+  State<StatefulWidget> createState() {
+    return _CounterPro();
+
+  }
+}
+
+class _CounterPro extends State<CounterPro>{
   @override
+  int counter = 0;
   Widget build(BuildContext context) {
+
+    increseCounter (){
+       setState(
+           (){
+             counter++;
+           }
+       );
+       // debugPrint('$counter');
+    }
     return Scaffold(
      appBar: AppBar(
        backgroundColor: Colors.purple.shade200,
        title: Text("Counter App"),
      ),
-      body: Column(
-        children: [
-          Text("0"),
-          Text("Clicked!")
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$counter'),
+            Text("Clicked!"),
+            FloatingActionButton(onPressed: (){})
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed:(){} ),
+      floatingActionButton: FloatingActionButton(onPressed:increseCounter,
+      child:Text("add"),),
     );
   }
 }
